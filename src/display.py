0faefb9edf90c4d761ea69ea4100e7aaf3ffd961
Miguel Sánchez de León Peque <peque@neosit.es>
2016-09-20 00:37:39 +0200

    Playing with canvas to display braille characters

---
+++ src/display.py
@@ -0,0 +1,36 @@
+"""
+A simple display to show Braille characters.
+"""
+import time
+import random
+from tkinter import Canvas, Tk
+
+
+def create_dot(canvas, x, y, r, fill=False):
+    return canvas.create_oval(x - r, y + r, x + r, y - r)
+
+
+class Display(Canvas):
+    def __init__(self, root):
+        super().__init__(root, width=500, height=500)
+        self.pack()
+        self.dots = {}
+
+        for j in range(4):
+            for i in range(2):
+                dot = create_dot(self, 100 * (i + 1), 100 * (j + 1), 30)
+                self.dots[i + j * 2] = dot
+
+    def show(self, code):
+        for key, value in zip(range(1, 9), code):
+            fill = '#000000' if int(value) else '#ffffff'
+            self.itemconfig(key, fill=fill)
+        self.update()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    display = Display(root)
+    for i in range(10):
+        display.show(''.join(str(random.randint(0, 1)) for x in range(8)))
+        time.sleep(1)
