f312a4e4777779d82bc97667f847c9def960f098
Juan Herrero <jn.herrerom@gmail.com>
2016-09-20 21:21:02 +0200

    Updated display with current and last 10 chars

--- braibook/display.py
+++ braibook/display.py
@@ -3,6 +3,7 @@ A simple display to show Braille characters.
 """
 import time
 import random
+from tables import LATIN
 from tkinter import Canvas, Tk
 
 
@@ -11,26 +12,46 @@ def create_dot(canvas, x, y, r, fill=False):
 
 
 class Display(Canvas):
-    def __init__(self, root, width=500, height=500):
+    def __init__(self, root, width=200, height=200):
         super().__init__(root, width=width, height=height)
         self.pack()
         self.dots = {}
+        self.last_chars = []
 
         for j in range(4):
             for i in range(2):
-                dot = create_dot(self, 100 * (i + 1), 100 * (j + 1), 30)
+                dot = create_dot(self, 30 * (i + 1), 30 * (j + 1), 9)
                 self.dots[i + j * 2] = dot
+        self.char = self.create_text(100, 75, text='', font=("Default", 40))
+        self.sentence = self.create_text(25, 150, text=self.last_chars,
+                                         font=("Default", 12),
+                                         anchor='w', width=160)   
 
-    def show(self, code):
-        for key, value in zip(range(8), code):
-            fill = '#000000' if int(value) else '#ffffff'
-            self.itemconfig(self.dots[key], fill=fill)
+    def show(self, char):
+        code = LATIN[char]
+        for dot in range(1,9):
+            fill = '#000000' if str(dot) in str(code) else '#ffffff'
+            self.itemconfig(self.dots[dot-1], fill=fill)
+            
+        self.delete(self.char)
+        self.char = self.create_text(130, 75, text=char, font=("Default", 60))
+        
+        self.__update_last_chars(char)
+        self.delete(self.sentence)
+        self.sentence = self.create_text(25, 160, text=self.last_chars, font=("Default", 12),
+                         anchor='w', width=160)      
         self.update()
-
+        
+    def __update_last_chars(self, char):
+        self.last_chars.append(char)
+        if len(self.last_chars) > 20:
+            self.last_chars[:10] = []
+            self.delete(self.sentence)
+        
 
 if __name__ == '__main__':
     root = Tk()
-    display = Display(root, 500, 500)
-    for i in range(10):
-        display.show(random.choice((0, 1)) for x in range(8))
-        time.sleep(1)
+    display = Display(root, width=200, height=200)
+    for i in range(50):
+        display.show(chr(random.randrange(97, 122)))
+        time.sleep(0.1)
