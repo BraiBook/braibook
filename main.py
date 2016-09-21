b3228508d0e034f6b3713467e3b1905999fa3880
Miguel Sánchez de León Peque <peque@neosit.es>
2016-09-21 19:52:36 +0200

    Update LATIN translation table

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ from braibook.display import Display
 if __name__ == '__main__':
     main = Tk()
     display = Display(main, width=200, height=200)
-    for c in 'hello world! this is a braille display...':
+    for c in 'Hello world! this is a braille display test...':
         display.show(c)
         time.sleep(0.5)
